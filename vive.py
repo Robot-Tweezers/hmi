348da98ecd2e9ba4cffd77117589d1edcdf25a8a
Tyler Keeling <tylerwkeeling@gmail.com>
2022-04-05 10:56:39 -0700

    Move vive to thread
    Will help with filtering later on

--- vive.py
+++ vive.py
@@ -1,18 +1,22 @@
 import time
 import sys
 from typing import Type
+import threading
 
 sys.path.append('triad_openvr/')
 import triad_openvr
 
 class Vive:
-    def __init__(self):
+    def __init__(self, interval=1/250, debug=False):
         self.v = triad_openvr.triad_openvr()
         self.v.print_discovered_objects()
 
         self.roll = 0
         self.pitch = 0
         self.yaw = 0
+
+        self.interval = interval
+        self.debug = debug
 
     def update(self):
         dat = self.v.devices["tracker_1"].get_pose_euler()
@@ -24,18 +28,30 @@ class Vive:
         except TypeError:
             pass
 
-        self.printstate()
+        if self.debug:
+            self.printstate()
 
     def printstate(self):
         txt = ""
         for each in [self.roll, self.pitch, self.yaw]:
-            txt += "%.4f" % each
+            txt += "%5.1f" % each
             txt += " "
         print("\r" + txt, end="")
 
+    def loop(self):
+        while True:
+            start = time.time()
+            self.update()
+            sleep_time = self.interval-(time.time()-start)
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+
+    def start(self):
+        self.t = threading.Thread(target=self.loop, daemon=True)
+        self.t.start()
+
 if __name__ == "__main__":
-    vive = Vive()
+    vive = Vive(debug=True)
+    vive.start()
     while True:
-        vive.update()
-        vive.printstate()
-        time.sleep(0.01)
+        continue
